24a3601bc44af3331391281d616fcc182c1aecfa
赵军 <zhaojun@300.cn>
2018-11-13 10:20:56 +0800

    add example

--- example/demo.lua
+++ example/demo.lua
@@ -12,11 +12,23 @@ local app = router.new()
 app.post("/",function(req,res)
     local a = req.param.a
     -- local a = req.param["a"]
-    res:render(a)
+    res:html("index.html",a)
+end)
+
+app.post("/json",function(req,res)
+    local a = req.param.a
+    -- local a = req.param["a"]
+    res:json(a)
+end)
+
+app.get("/hello",function(req,res)
+    local a = req.param.a
+    -- local a = req.param["a"]
+    res:html("index.html",a)
 end)
 
 app.err(function(req,res)
-    res:render("page err..")
+    res:render("server err..")
 end)
 
 app.go()
--- lua/response.lua
+++ lua/response.lua
@@ -6,12 +6,20 @@
 -- To change this template use File | Settings | File Templates.
 --
 local json = require("cjson.safe")
+local template = require "resty.template"
 
 local ngx = ngx
 local _M = {}
 local mt = {
     __index = _M
 }
+local function set_header(key,value)
+    ngx.header[key] = value
+end
+local function default_header()
+    set_header('Content-Type', 'text/html; charset=UTF-8')
+end
+
 
 function _M.new()
     local self = {}
@@ -19,23 +27,21 @@ function _M.new()
 end
 
 function _M.render(self,content)
-    ngx.say(content)
+    default_header()
+    ngx.print(content)
 end
 
 --- 返回html
-function _M.html(self,data)
-    self:set_header('Content-Type', 'text/html; charset=UTF-8')
-    self:render(data)
+function _M.html(self,view,data)
+    template.render(view,data)
 end
 
 -- 返回json
 function _M.json(self,content)
-    self:set_header('Content-Type', 'application/json; charset=utf-8')
-    ngx.render( json.json_encode(content))
+    set_header('Content-Type', 'application/json; charset=utf-8')
+    ngx.print( json.json_encode(content))
 end
 
-function _M.set_header(key,value)
-    ngx.header[key] = value
-end
+
 return _M
 
